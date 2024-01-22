c0107dd50c92ffac1e103c09c8e870d6c54be0ad
OptiJava <OptiJava@protonmail.com>
2024-01-22 15:51:02 +0800

    chore: config class

--- main.py
+++ main.py
@@ -4,6 +4,55 @@ import sys
 logger = logging.getLogger('mcdr_debugger')
 
 
+class Config:
+    debug = False
+
+    ##########
+    # minecraft server configurations
+    core_server_url = 'https://piston-data.mojang.com/v1/objects/8dd1a28015f51b1803213892b50b7b4fc76e594d/server.jar'
+    # will download server core file from this url
+    # Default: 1.20.4 vanilla server url from mojang
+    auto_eula = False
+    # should I agree the eula automatically?
+    # Default: False
+
+    ##########
+    # mcdreforged configurations
+    plugins = []
+    # did you need other mcdr plugins? add the download url here!
+    # Default: empty
+    python_path = ''
+    # python executable file path
+    # Default: blank (will use 'python3 ...' command in default shell later)
+    pip_path = ''
+    # pip executable file path
+    # Default: blank (will use 'pip3 ...' command in default shell later)
+    env_path = './env'
+    # will init mcdr in this path
+    # Default: ./env
+
+    ##########
+    # mcdreforged plugin packaging configurations
+    method = 'mcdr_command'
+    # how can I package your plugin?
+    # Options:
+    #   mcdr_command: use mcdr pack command
+    #   folder: move the whole folder to the 'plugins' folder
+    #   single_file: move the single file to the 'plugins' folder
+    # Default: mcdr_command
+    plugin_code_path = './code'
+    # the whole plugin code should in this path
+    # you must change it
+    # if you choose 'mcdr_command', we will execute pack command in this folder
+    # if you choose 'folder', we will move this folder to 'plugins' folder
+    # if you choose 'single_file', this path must be a file, maybe '.py' or '.mcdr'
+    # Default: ./code
+    mcdr_pack_extra_options = '--ignore-patterns __pycache__'
+    # did you want to add some extra options when packing plugin by mcdr pack command?
+    # see mcdr official docs for more details
+    # Default: --ignore-patterns __pycache__ (will ignore __pycache__)
+
+
 def main(args: list):
     if args[0] == 'init':
         pass
