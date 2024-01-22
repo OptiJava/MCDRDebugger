f4121be5cd94f96b07cbe78843d05b94f5c44267
OptiJava <OptiJava@protonmail.com>
2024-01-22 15:10:42 +0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import logging
+import sys
+
+logger = logging.getLogger('mcdr_debugger')
+
+
+def main(args: list):
+    if args[0] == 'init':
+        pass
+
+
+if __name__ == '__main__':
+    main(sys.argv)
